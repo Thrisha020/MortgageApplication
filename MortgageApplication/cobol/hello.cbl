66cbdd552ad74f8755354cdea62c3710f7080f6c
raguraman <raguraman.mps6066@gmail.com>
2025-01-25 16:54:20 +0530

    my commit at 25th

--- MortgageApplication/cobol/hello.cbl
+++ MortgageApplication/cobol/hello.cbl
@@ -3,7 +3,7 @@
        ENVIRONMENT DIVISION.
       *hyeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
        PROCEDURE DIVISION.
-           DISPLAY 'Hello def'.
+           DISPLAY 'Hello ra'.
            DISPLAY '12345bdxf'
            STOP RUN.
 
