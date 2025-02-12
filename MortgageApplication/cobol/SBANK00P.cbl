817afa235d2d03755f6c154bf2c18427d22f6a61
raguraman <raguraman.mps6066@gmail.com>
2025-02-12 16:58:47 +0530

    firstcommit

--- MortgageApplication/cobol/SBANK00P.cbl
+++ MortgageApplication/cobol/SBANK00P.cbl
@@ -3,7 +3,7 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  PARM-1.
+       01  PARM-1
            05  CALL-FEEDBACK     PIC XX.
        01  FIELDS
            05  INPUT-1           PIC X(10).
